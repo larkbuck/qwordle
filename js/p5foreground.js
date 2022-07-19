824525751249760556d4cfb29061654d805acd9a
larkbuck <lark.buckingham@sjsu.edu>
2022-07-19 15:56:25 -0700

    changed text color, added temporary "YES" win

--- js/p5foreground.js
+++ js/p5foreground.js
@@ -10,14 +10,17 @@ let canvasDivInstance = function(p) { // p could be any variable name
   p.setup = function() {
     p.canvas = p.createCanvas(canvasWidth, canvasHeight);
     p.canvas.parent('overlayCanvas');
-    // p.background(100, 100, 100);
+    p.textSize(width / 5);
+    p.textAlign(CENTER, CENTER);
   };
 
   p.draw = function() {
     p.clear();
 
     if (win) {
-      p.ellipse(100, 200, 100);
+      // p.text(canvasWidth / 3, canvasHeight / 6);
+      // p.ellipse(canvasWidth / 2, canvasHeight / 2, 100);
+      p.text('YES', canvasWidth / 2, canvasHeight / 2);
     }
   };
 
