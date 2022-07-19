5f65dff5b1a8f94b8ffcb8cb36a9cc899898c034
larkbuck <lark.buckingham@sjsu.edu>
2022-07-19 15:30:04 -0700

    added falling triangle background

--- js/triangles.js
+++ js/triangles.js
@@ -1,11 +1,12 @@
 function createTriangleArray() { // triangle animation
   for (let i = 0; i < numOfTriangles; i++) {
-    triangleArray.push(new Triangle(20, 50));
-    triangleArray.push(new Triangle(windowWidth / 10, windowWidth / 10 + 10));
-    triangleArray.push(new Triangle(windowWidth / 5, windowWidth / 5 + 10));
-    triangleArray.push(new Triangle(windowWidth * 3 / 5, windowWidth * 3 / 5 + 10));
-    triangleArray.push(new Triangle(windowWidth * 5 / 6, windowWidth * 5 / 6 + 10));
-    triangleArray.push(new Triangle(windowWidth - 33, windowWidth - 50));
+    triangleArray.push(new Triangle(0, windowWidth));
+  //   triangleArray.push(new Triangle(20, 50));
+  //   triangleArray.push(new Triangle(windowWidth / 10, windowWidth / 10 + 10));
+  //   triangleArray.push(new Triangle(windowWidth / 5, windowWidth / 5 + 10));
+  //   triangleArray.push(new Triangle(windowWidth * 3 / 5, windowWidth * 3 / 5 + 10));
+  //   triangleArray.push(new Triangle(windowWidth * 5 / 6, windowWidth * 5 / 6 + 10));
+  //   triangleArray.push(new Triangle(windowWidth - 33, windowWidth - 50));
   }
 
   triangleArrayLength = triangleArray.length;
@@ -32,21 +33,28 @@ class Triangle {
     this.y = random(0, windowHeight);
     // this.y = random(windowHeight + 50, windowHeight * 2);
     this.speedX = 0;
-    this.speedY = -4;
+    this.speedY = -1;
     this.accelX = 0;
-    this.accelY = random(-0.0080, -0.0010);
+    this.accelY = random(-0.0050, -0.0010);
+    this.rotation = 0;
+    this.rotationSpeed = random(-.03, .03);
     this.diam = random(4, 8);
-    this.c = color(random(222, 255), random(222, 255), random(222, 255));
+    this.c = color(random(180, 215), random(60, 70), random(110, 200), 200);
   }
 
   drawTriangle() {
     noStroke();
     fill(this.c);
-    ellipse(this.x, this.y, this.diam, this.diam);
+    push();
+    translate(this.x, this.y);
+    this.rotation += this.rotationSpeed;
+    rotate(this.rotation);
+    triangle(-10, 10, 0, -10, 10, 10);
+    pop();
   }
 
   moveTriangle() {
-    this.y = this.y + this.speedY;
+    this.y = this.y - this.speedY;
     this.speedY = this.speedY + this.accelY;
 
     this.x = this.x + this.speedX;
@@ -60,14 +68,15 @@ class Triangle {
   }
 
   checkBounds() {
-    if (this.y < -20) {
-      this.y = random(windowHeight + 10, windowHeight + 100);
-      this.speedY = -4;
+    if (this.y > windowHeight + 20) {
+      this.y = random(-10, -20);
+      // this.y = random(windowHeight + 10, windowHeight + 100);
+      this.speedY = -1;
       this.x = random(this.xStart, this.xEnd);
       this.diam = random(3, 7);
       this.speedX = 0;
       this.accelX = 0;
-      this.c = color(random(222, 255), random(222, 255), random(222, 255));
+      this.c = color(random(180, 215), random(60, 70), random(110, 200), 200);
     }
   }
 }
